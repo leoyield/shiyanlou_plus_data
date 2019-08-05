f71c6652bb5a4b8aba53373f2c78b157d201226e
leo <yeleihot@sina.com>
2019-08-05 17:35:17 +0800

    success

--- houseprice.py
+++ houseprice.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
 def clean():
     df = pd.read_csv('beijing_house_price.csv')
@@ -14,20 +15,19 @@ def clean():
 
 def beijin(n):
     df = clean()
-    split_num = int(len(df)*0.7)
-    X_train = df.iloc[:split_num,1:]
-    y_train = df.iloc[:split_num,0]
-    X_test = df.iloc[split_num:,1:]
-    y_test = df.iloc[split_num:,0]
-
+    X_train, X_test, y_train, y_test = train_test_split(df.iloc[:,1:],
+                                       df.iloc[:,0], test_size = 0.3,
+                                       random_state = 10)
+    y_train = y_train.values.reshape(len(y_train), 1)
+    y_test = y_test.values.reshape(len(y_test), 1)
     poly_n = PolynomialFeatures(n)
     poly_X_train = poly_n.fit_transform(X_train)
     poly_X_test = poly_n.fit_transform(X_test)
     model = LinearRegression()
-    model.fit(poly_X_train, y_train.values.reshape(len(y_train), 1))
+    model.fit(poly_X_train, y_train)
     result = model.predict(poly_X_test)
-
-    return result #poly_X_train.shape, y_train.values.reshape(len(y_train), 1).shape
+    mae = mean_absolute_error(result, y_test)
+    return mae
 print(clean().head())
 print('-'*50)
-print(beijin(1))
+print(beijin(5))
